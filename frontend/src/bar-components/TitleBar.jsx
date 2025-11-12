f9c3cbbe6ee8f562f16e81a61f6e37ff32cdff59
uiskskkekekk <angus.lu0611@gmail.com>
2025-11-12 22:04:47 +0800

    REMOVE: redundant style

--- frontend/src/bar-components/TitleBar.jsx
+++ frontend/src/bar-components/TitleBar.jsx
@@ -3,9 +3,7 @@ import '../styles/bar-components/TitleBar.css'
 const TitleBar = () => {
     return(
         <div className="title-bar">
-            <div className="title-bar-left">
-                {/* <MacNavButton /> */}
-            </div>
+            <div className="title-bar-left" />
         </div>
     )
 }
